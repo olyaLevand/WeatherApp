6ec87e959367a2f7c08aad1071f64cb9a81f9deb
Olha <olha.levandivska@embrox.com>
2023-05-17 19:23:23 +0300

    Initial Commit

---
+++ WatherApp/WatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatherAppApp.swift
+//  WatherApp
+//
+//  Created by Olya Levandivska on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
